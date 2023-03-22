0b3e392fe01918ec1baf3e436b7b137bbe80ec97
Kaylane <kaylanef3rreira@gmail.com>
2023-03-22 14:57:19 -0300

    adicionamento de temperatura e refresh

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import Informations from './components/Informations';
 
 export default function App() {
+
+  const [darkTheme, setDarkTheme] = useState(true)
+  const [currentTemperature, setCurrentTemperature] = useState('27')
+  const [location, setLocation] = useState('BR, Conselheiro Lafaiete')
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: darkTheme ? '#232634' : '#f2f2f2',
+      alignItems: 'center',
+    },
+    temperature: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      marginTop: 10,
+    },
+    temperatureText: {
+      color: darkTheme ? '#e0e0e0' : 'black',
+      fontSize: 40
+    },
+    refreshBtn: {
+      position: 'absolute',
+      margin: 48,
+      alignSelf: 'flex-start'
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.refreshBtn}>
+        <Ionicons name="refresh" size={32} color={darkTheme ? 'white' : 'black'} />
+      </TouchableOpacity>
+      
+      <Ionicons name="sunny-outline" style={{ marginTop: 64 }} size={48} color={darkTheme ? 'white' : 'black'} />
+      <View style={styles.temperature}>
+        <Text style={styles.temperatureText}>{currentTemperature}</Text>
+        <Text style={[styles.temperatureText, { fontSize: 16 }]}>°C</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ components/Informations.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const Informations = (props) => {
+
+  const styles = StyleSheet.create({
+
+  });
+
+  return (
+    <View styles={styles.Informations}>
+
+    </View>
+  )
+}
+
+export default Informations
