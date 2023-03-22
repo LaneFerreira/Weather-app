9946c82f8abf18d2fe9381a6ec0e419278c60d1a
Kaylane <kaylanef3rreira@gmail.com>
2023-03-22 16:17:05 -0300

    adicionamento de local, hora, vento e umidade

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import Informations from './components/Informations';
+import React from 'react';
+import Informations from './components/Informations'
+import { Feather } from '@expo/vector-icons';
 
 export default function App() {
 
   const [darkTheme, setDarkTheme] = useState(true)
   const [currentTemperature, setCurrentTemperature] = useState('27')
-  const [location, setLocation] = useState('BR, Conselheiro Lafaiete')
+  const [location, setLocation] = useState('BR - Conselheiro Lafaiete')
+  const [currentHour, setCurrentHour] = useState('13:00')
+
+  const [wind, setWind] = useState('65')
+  const [umidity, setUmiduty] = useState('80')
 
   const styles = StyleSheet.create({
     container: {
@@ -29,6 +35,9 @@ export default function App() {
       position: 'absolute',
       margin: 48,
       alignSelf: 'flex-start'
+    },
+    info: {
+      flexDirection: 'row',
     }
   });
 
@@ -37,14 +46,33 @@ export default function App() {
       <TouchableOpacity style={styles.refreshBtn}>
         <Ionicons name="refresh" size={32} color={darkTheme ? 'white' : 'black'} />
       </TouchableOpacity>
-      
+
       <Ionicons name="sunny-outline" style={{ marginTop: 64 }} size={48} color={darkTheme ? 'white' : 'black'} />
       <View style={styles.temperature}>
         <Text style={styles.temperatureText}>{currentTemperature}</Text>
         <Text style={[styles.temperatureText, { fontSize: 16 }]}>°C</Text>
       </View>
+
+      <Text style={[styles.temperatureText, { fontSize: 16 }]}>{location}, {currentHour}</Text>
+
+      <Text style={{ fontSize: 56, color: 'white' }}>_</Text>
+
+      <View style={styles.info}>
+        <View style={{ alignItems: 'center' }}>
+          <Feather name="wind" size={24} color="white" />
+          <Informations title={'Vento'} value={wind + ' km/h'}></Informations>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Ionicons name="water-outline" size={24} color="white" />
+          <Informations title={'Umidade'} value={umidity + '%'}></Informations>
+        </View>
+      </View>
+
+      <View>
+        <View>
+
+        </View>
+      </View>
     </View>
   );
 }
-
-
--- components/Informations.js
+++ components/Informations.js
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
 
 const Informations = (props) => {
 
   const styles = StyleSheet.create({
-
+    informations: {
+      alignItems: 'center',
+      margin: 10,
+      minWidth: 150,
+    },
+    text: {
+      color: '#e8e8e8',
+      margin: 5,
+      marginLeft: 16,
+      fontSize: 16
+    }
   });
 
   return (
-    <View styles={styles.Informations}>
-
+    <View styles={styles.informations}>
+      <Text style={styles.text}>{props.title}</Text>
+      <Text style={styles.text}>{props.value}</Text>
     </View>
   )
 }
